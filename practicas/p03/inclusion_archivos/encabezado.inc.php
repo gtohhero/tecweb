c5918e6f8249d5bc2e8f0c7eb5a8c2bb304828d0
gtohhero_ <franciscozatarain1@gmail.com>
2025-01-29 21:42:32 -0600

    encabezado.inc.php corregido

--- practicas/p03/inclusion_archivos/encabezado.inc.php
+++ practicas/p03/inclusion_archivos/encabezado.inc.php
@@ -15,7 +15,7 @@
 
     <body>
         <?php
-            $variableext=«;
+            $variableext;
             echo "<div><h1 style=\"border-width:5;border-style:double;background-color:#ffcc99;\">
             Bienvenido en el sitio $variable1 </h1>";
             echo "<h3> $variableext</h3>";
