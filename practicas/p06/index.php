363378861d34d3b355551a356229c1beccec35e7
gtohhero_ <franciscozatarain1@gmail.com>
2025-02-05 14:28:54 -0600

    index.php modificado según ejercicio 1; función es_multiplo7y5 agregado en src/funciones.php; ejercicio 1 ahora usa la función anterior

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -9,17 +9,10 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
+        require_once __DIR__.'/src/funciones.php';
         if(isset($_GET['numero']))
         {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            es_multiplo7y5($_GET['numero']);
         }
     ?>
 
