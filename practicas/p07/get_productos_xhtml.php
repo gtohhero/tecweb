f1791fd5fcac0cb25c8a6b6802ecc750a5b33d10
gtohhero_ <franciscozatarain1@gmail.com>
2025-02-17 15:40:08 -0600

    p07/get_productos_xhtml.php fue corregido con tope siendo un valor no numérico

--- practicas/p07/get_productos_xhtml.php
+++ practicas/p07/get_productos_xhtml.php
@@ -1,74 +1,72 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Producto</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <h3>PRODUCTO</h3>
-    <br/>
-
     <?php
-        if(isset($_GET['tope']))
-        {
-            $tope = $_GET['tope'];
+    if(isset($_GET['tope'])) {
+        $tope = $_GET['tope'];
+        if (!is_numeric($tope)) {
+            die('No hay productos con unidades ' . htmlspecialchars($tope));
         }
-        else
-        {
-            die('Parámetro "tope" no detectado...');
-        }
+    } else {
+        die('Parámetro "tope" no detectado...');
+    }
 
-        /** SE CREA EL OBJETO DE CONEXION */
-        $link = new mysqli('localhost', 'root', 'daSH1NE_Zz!', 'marketzone');
+    if (!empty($tope)) {
+        @$link = new mysqli('localhost', 'root', 'daSH1NE_Zz!', 'marketzone');
 
         if ($link->connect_errno) {
-            die('<script>alert("Falló la conexión: ' . $link->connect_error . '");</script>');
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
         }
 
-        $stmt = $link->prepare("SELECT * FROM productos WHERE unidades <= ?");
-        $stmt->bind_param("i", $tope);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            echo '<table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Marca</th>
-                            <th scope="col">Modelo</th>
-                            <th scope="col">Precio</th>
-                            <th scope="col">Unidades</th>
-                            <th scope="col">Detalles</th>
-                            <th scope="col">Imagen</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-
-            while ($row = $result->fetch_assoc()) {
-                echo '<tr>
-                        <th scope="row">' . htmlspecialchars($row['id']) . '</th>
-                        <td>' . htmlspecialchars($row['nombre']) . '</td>
-                        <td>' . htmlspecialchars($row['marca']) . '</td>
-                        <td>' . htmlspecialchars($row['modelo']) . '</td>
-                        <td>' . htmlspecialchars($row['precio']) . '</td>
-                        <td>' . htmlspecialchars($row['unidades']) . '</td>
-                        <td>' . htmlspecialchars($row['detalles']) . '</td>
-                        <td><img src="'. htmlspecialchars($row['imagen']) .'" width="100"></td>
-                    </tr>';
-            }
-
-            echo '</tbody></table>';
-        } else {
-            echo '<script>alert("No hay productos con stock menor o igual a '. $tope .'");</script>';
+        if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) {
+            $productos = $result->fetch_all(MYSQLI_ASSOC);
+            $result->free();
         }
 
-        $stmt->close();
         $link->close();
+    }
     ?>
-</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Productos</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <h3>PRODUCTOS</h3>
+        <br/>
+        <?php if (!empty($productos)) : ?>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Unidades</th>
+                        <th scope="col">Detalles</th>
+                        <th scope="col">Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($productos as $producto) : ?>
+                        <tr>
+                            <th scope="row"> <?= $producto['id'] ?> </th>
+                            <td> <?= $producto['nombre'] ?> </td>
+                            <td> <?= $producto['marca'] ?> </td>
+                            <td> <?= $producto['modelo'] ?> </td>
+                            <td> <?= $producto['precio'] ?> </td>
+                            <td> <?= $producto['unidades'] ?> </td>
+                            <td> <?= $producto['detalles'] ?> </td>
+                            <td><img src="<?= $producto['imagen'] ?>"/></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else : ?>
+            <script>
+                alert('No hay productos con unidades <?= $tope ?>');
+            </script>
+        <?php endif; ?>
+    </body>
 </html>
