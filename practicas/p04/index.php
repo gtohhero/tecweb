19473e190977b2a2a435609d43b14dd9042dbd6d
gtohhero_ <franciscozatarain1@gmail.com>
2025-02-18 08:10:05 -0600

    Práctica 8 agregada: Se hicieron las correcciones de marcado y se agregó el ícono de validación XHTML

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -36,9 +36,9 @@
     <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sige:</p>
     <p>
-        $a = "ManejadorSQL"; <br>
-        $b = 'MySQL'; <br>
-        $c = &$a;
+        $a = "ManejadorSQL"; <br/>
+        $b = 'MySQL'; <br/>
+        $c = &amp;$a;
     </p>
     
     <?php
@@ -54,13 +54,13 @@
 
         echo '<li>Agrega el código actual las siguientes asignaciones:</li>';
 
-        echo '<br><div style="margin-left: -40px;">$a = "PHP server" <br>';
+        echo '<br/><div style="margin-left: -40px;">$a = "PHP server" <br/>';
         echo '$b = &$a</div>';
 
         $a = "PHP server";
         $b = &$a;
 
-        echo '<br><li>Vuelve a mostrar el contenido de cada uno</li>';
+        echo '<br/><li>Vuelve a mostrar el contenido de cada uno</li>';
         echo '<p>Valor de $a es: ' . "$a" . '</p>';
         echo '<p>Valor de $b es: ' . "$b" . '</p>';
         echo '<p>Valor de $c es: ' . "$c" . '</p>';
@@ -78,19 +78,19 @@
     <h2>Ejercicio 3</h2>
     <p>Muestra el contenido de cada variable inmediatamente después de cada asignación, verificar la evolución del tipo de estas variables (imprime todos los componentes del arreglo):</p>
     <p>
-        $a = "PHP5"; <br>
-        $z[] = &$a; <br>
-        $b = "5a version de PHP"; <br>
-        $c = $b*10; <br>
-        $a . = $b; <br>
-        $b * = $c; <br>
+        $a = "PHP5"; <br/>
+        $z[] = &amp;$a; <br/>   
+        $b = "5a version de PHP"; <br/>
+        $c = $b*10; <br/>
+        $a . = $b; <br/>
+        $b * = $c; <br/>
         $z[0] = "MySQL";
     </p>
 
     <?php
         $a = "PHP5";
         echo '<ul>';
-        echo '<li>Valor de $a es: </li>' . "$a" . "<br>";
+        echo '<li>Valor de $a es: </li>' . "$a" . "<br/>";
         var_dump($a);
 
         $z[] = &$a;
@@ -98,23 +98,23 @@
         var_dump($z);
 
         $b = "5a version de PHP";
-        echo '<li>Valor de $b es: </li>' . "$b" . "<br>";
+        echo '<li>Valor de $b es: </li>' . "$b" . "<br/>";
         var_dump($b);
 
         $c = $b * 10;
-        echo '<li>Valor de $c es: </li>' . "$c" . "<br>";
+        echo '<li>Valor de $c es: </li>' . "$c" . "<br/>";
         var_dump($c);
 
         $a .= $b;
-        echo '<li>Valor de $a es: </li>' . "$a" . "<br>";
+        echo '<li>Valor de $a es: </li>' . "$a" . "<br/>";
         var_dump($a);
 
         $b *= $c;
-        echo '<li>Valor de $b es: </li>' . "$b" . "<br>";
+        echo '<li>Valor de $b es: </li>' . "$b" . "<br/>";
         var_dump($b);
 
         $z[0] = "MySQL";
-        echo '<li>Valor de $z[0] es: </li>' . "$z[0]" . "<br>";
+        echo '<li>Valor de $z[0] es: </li>' . "$z[0]" . "<br/>";
         var_dump($z[0]);
         echo '</ul>';
     ?>
@@ -126,16 +126,16 @@
         global $a, $b, $c, $z;
         
         echo '<ul>';
-        echo '<li>Valor de $a es: </li>' . "$a" . "<br>";
+        echo '<li>Valor de $a es: </li>' . "$a" . "<br/>";
         var_dump($a);
 
         echo '<li>Valor de $z[] es: </li>';
         var_dump($z);
 
-        echo '<li>Valor de $c es: </li>' . "$c" . "<br>";
+        echo '<li>Valor de $c es: </li>' . "$c" . "<br/>";
         var_dump($c);
 
-        echo '<li>Valor de $z[0] es: </li>' . "$z[0]" . "<br>";
+        echo '<li>Valor de $z[0] es: </li>' . "$z[0]" . "<br/>";
         var_dump($z[0]);
         echo '</ul>';
 
@@ -145,32 +145,32 @@
     <h2>Ejercicio 5</h2>    
     <p>Dar el valor de las variables $a, $b, $c al final del siguiente script:</p>
     <p>
-        $a = "7 personas"; <br>
-        $b = (integer) $a; <br>
-        $a = "9E3"; <br>
-        $c = (double) $a; <br>
+        $a = "7 personas"; <br/>
+        $b = (integer) $a; <br/>
+        $a = "9E3"; <br/>
+        $c = (double) $a; <br/>
     </p>
 
     <?php
         $a = "7 personas";
         echo '<ul>';
-        echo '<li>Valor de $a es: </li>' . "$a" . "<br>";
-        echo gettype($a) . "<br>";
+        echo '<li>Valor de $a es: </li>' . "$a" . "<br/>";
+        echo gettype($a) . "<br/>";
         var_dump($a);
 
         $b = (integer) $a;
-        echo '<li>Valor de $b es: </li>' . "$b" . "<br>";
-        echo gettype($b) . "<br>";
+        echo '<li>Valor de $b es: </li>' . "$b" . "<br/>";
+        echo gettype($b) . "<br/>";
         var_dump($b);
 
         $a = "9E3";
-        echo '<li>Valor de $a es: </li>' . "$a" . "<br>";
-        echo gettype($a) . "<br>";
+        echo '<li>Valor de $a es: </li>' . "$a" . "<br/>";
+        echo gettype($a) . "<br/>";
         var_dump($a);
 
         $c = (double) $a;
-        echo '<li>Valor de $c es: </li>' . "$c" . "<br>";
-        echo gettype($c) . "<br>";
+        echo '<li>Valor de $c es: </li>' . "$c" . "<br/>";
+        echo gettype($c) . "<br/>";
         var_dump($c);
         echo '</ul>';
 
@@ -181,11 +181,11 @@
     <p>Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e, $f y muéstralas usando la función var_dump(datos).</p>
     <p>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e en uno que se pueda mostrar con un echo:</p>
     <p>
-        $a = "0"; <br>
-        $b = "TRUE"; <br>
-        $c = FALSE; <br>
-        $d = ($a OR $b); <br>
-        $e = ($a AND $c); <br>
+        $a = "0"; <br/>
+        $b = "TRUE"; <br/>
+        $c = FALSE; <br/>
+        $d = ($a OR $b); <br/>
+        $e = ($a AND $c); <br/>
         $f = ($a XOR $b);
     </p>
 
@@ -215,8 +215,8 @@
         echo '<li>Valor de $f es: </li>';
         var_dump($f);
 
-        echo '<li>Valor de $c es: </li>' . var_export($c, true) . '<br>';
-        echo '<li>Valor de $e es: </li>' . var_export($e, true) . '<br>';
+        echo '<li>Valor de $c es: </li>' . var_export($c, true) . '<br/>';
+        echo '<li>Valor de $e es: </li>' . var_export($e, true) . '<br/>';
         echo '</ul>';
     ?>
 
@@ -230,14 +230,18 @@
 
     <?php
         echo "<ul>";
-        echo "<li>Versión de Apache: " . $_SERVER['SERVER_SOFTWARE'] . "</li> </br>";
+        echo "<li>Versión de Apache: " . $_SERVER['SERVER_SOFTWARE'] . "</li> </br/>";
         $_SERVER['PHP_VERSION'] = phpversion();
-        echo "<li>Versión de PHP: " . $_SERVER['PHP_VERSION'] . "</li> </br>";
+        echo "<li>Versión de PHP: " . $_SERVER['PHP_VERSION'] . "</li> </br/>";
         $_SERVER['SERVER_OS'] = php_uname();
-        echo "<li>Nombre del sistema operativo (servidor): " . $_SERVER['SERVER_OS'] . "</li> </br>";
+        echo "<li>Nombre del sistema operativo (servidor): " . $_SERVER['SERVER_OS'] . "</li> </br/>";
         echo "<li>Idioma del navegador: " . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "</li>";
         echo "</ul>";
     ?>
 
+    <p>
+        <a href="https://validator.w3.org/markup/check?uri=referer"><img
+        src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    </p>
 </body>
 </html>
